5f1ba736d5288bdaad5653c89aaba016dc11dd39
giuli <giulia.polverini.gp1@gmail.com>
2025-10-22 11:04:29 +0200

    uno

--- prova/src/main/java/prova/Studente.java
+++ prova/src/main/java/prova/Studente.java
@@ -1,5 +1,40 @@
 package prova;
 
 public class Studente {
-
+	
+	private String matricola;
+	private String nome;
+	private String cognome;
+	
+	
+	
+	public Studente(String matricola, String nome, String cognome) {
+		super();
+		this.matricola = matricola;
+		this.nome = nome;
+		this.cognome = cognome;
+	}
+	
+	
+	public String getMatricola() {
+		return matricola;
+	}
+	public void setMatricola(String matricola) {
+		this.matricola = matricola;
+	}
+	public String getNome() {
+		return nome + "!";
+		
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	
 }
